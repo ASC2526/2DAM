faa3dc9976655cb65b6146976a8f0b46096aa21e
ASC2526 <alesancor7@alu.edu.gva.es>
2025-11-12 19:08:03 +0100

    update myformulario

--- Flutter/Practica4/comarcasgui/lib/screens/my_formulario.dart
+++ Flutter/Practica4/comarcasgui/lib/screens/my_formulario.dart
@@ -1,45 +1,119 @@
 import 'package:flutter/material.dart';
-import 'launcher_screen.dart'; 
+import 'launcher_screen.dart';
 
-class MyFormulario extends StatelessWidget {
+class MyFormulario extends StatefulWidget {
   const MyFormulario({super.key});
+
+  @override
+  State<MyFormulario> createState() => _MyFormularioState();
+}
+
+class _MyFormularioState extends State<MyFormulario> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Iniciar sesión")),
       body: Padding(
         padding: const EdgeInsets.all(16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const TextField(
-              decoration: InputDecoration(
-                labelText: "Usuario",
-                border: OutlineInputBorder(),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Iniciar Sesión",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
-            ),
-            const SizedBox(height: 16),
-            const TextField(
-              obscureText: true, 
-              decoration: InputDecoration(
-                labelText: "Contraseña",
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const LauncherScreen(),
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  const Icon(Icons.email_rounded),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: TextFormField(
+                      controller: _emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: 'Correo electrónico',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'No puede estar vacío';
+                        }
+                        if (!value.contains('@')) {
+                          return 'La dirección de correo no es válida';
+                        }
+                        return null;
+                      },
+                    ),
                   ),
-                );
-              },
-              child: const Text("Entrar"),
-            ),
-          ],
+                ],
+              ),
+              const SizedBox(height: 16),
+              Row(
+                children: [
+                  const Icon(Icons.lock),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: TextFormField(
+                      controller: _passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Contraseña',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'No puede estar vacío';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    const correoValido = "pepito@yahoo.com";
+                    const passwordValida = "1234";
+
+                    if (_emailController.text == correoValido &&
+                        _passwordController.text == passwordValida) {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const LauncherScreen(),
+                        ),
+                      );
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text("Correo o contraseña incorrectos"),
+                          backgroundColor: Colors.redAccent,
+                        ),
+                      );
+                    }
+                  }
+                },
+                child: const Text("Iniciar sesión"),
+              ),
+            ],
+          ),
         ),
       ),
     );
